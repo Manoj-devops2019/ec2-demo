1ce8db6012c4edf237d1f1268fe4b6972e455f20
magarwala <manojagarwala.analyst@gmail.com>
2020-04-30 08:51:12 +0000

    commit

--- provider.tf
+++ provider.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "ec2-server" {
+  instance_type = "t2.micro"
+  ami = "ami-0c322300a1dd5dc79"
+  tags = {
+  Name = "first-public-instance"
+  }
+}
