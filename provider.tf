b2ab650de24693f347e2f1397dbb0280795fad30
Manoj-devops2019 <59191735+Manoj-devops2019@users.noreply.github.com>
2020-05-11 11:13:24 +0530

    commit

--- provider.tf
+++ provider.tf
@@ -1,10 +1,12 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAXXYSFF7BKO2WYYVX"
+  secret_key = "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"
 }
 
 resource "aws_instance" "ec2-server" {
   instance_type = "t2.micro"
-  ami = "ami-098f16afa9edf40be"
+  ami = "ami-0c322300a1dd5dc79"
   tags = {
   Name = "first-public-instance"
   }
